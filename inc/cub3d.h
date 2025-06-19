2961415245694d9a54900ac1fc886a08bc2ddb46
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2025-06-19 12:25:48 +0000

    refactor: reorganize data structures in cub3d.h for clarity and consistency
    Co-authored-by: lareii <lareii@proton.me>

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ahekinci <ahekinci@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 21:17:24 by ebabaogl          #+#    #+#             */
-/*   Updated: 2025/06/19 02:31:32 by ahekinci         ###   ########.fr       */
+/*   Updated: 2025/06/19 15:23:02 by ahekinci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,52 @@
 
 # define CIRCLE_COLOR 0xFF0000
 
-typedef struct s_data { // for testing
+typedef struct s_data {
 	void *mlx;
 	void *win;
-	void *img;
-	char *addr;
-	int bpp;
-	int line_length;
-	int endian;
+	t_image img;
 	int x;
 	int y;
+	s_key_status key_status;
+} t_data;
+
+typedef struct s_image {
+	void *img_ptr;
+	char *data;
+	int width;
+	int height;
+	int bpp;
+	int size_line;
+	int endian;
+} t_image;
+
+typedef struct s_key_status {
 	int key_w;
 	int key_a;
 	int key_s;
 	int key_d;
-} t_data;
+} t_key_status;
+
+typedef struct s_circle {
+	int x;
+	int y;
+	int radius;
+	int color;
+} t_circle;
+
+typedef struct s_rectangle {
+	int x;
+	int y;
+	int width;
+	int height;
+	int color;
+} t_rectangle;
+
+
+// for example usage:
+// int draw_circle(t_circle circle)
+// draw_circle((t_circle){.x = 400, .y = 300, .radius = 50, .color = CIRCLE_COLOR});
+
 
 void	draw_pixel_on_image(t_data *data, int x, int y, int color);
 void	draw_circle_on_image(t_data *data, int radius, int x, int y);
