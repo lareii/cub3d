c692e68c808985a74e4dc10eb647d07b74747682
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2025-06-19 12:38:50 +0000

    fix: update draw pixel function according to norm rules
    Co-authored-by: lareii <lareii@proton.me>

--- src/draw.c
+++ src/draw.c
@@ -6,27 +6,32 @@
 /*   By: ahekinci <ahekinci@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 01:26:50 by ahekinci          #+#    #+#             */
-/*   Updated: 2025/06/19 02:25:42 by ahekinci         ###   ########.fr       */
+/*   Updated: 2025/06/19 12:37:46 by ahekinci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 #include "cub3d.h"
 
-void draw_pixel_on_image(t_data *data, int color, int x, int y)
+void	draw_pixel_on_image(t_data *data, int color, int x, int y)
 {
+	char	*dst;
+
 	if (x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT)
-		return;
-	char *dst = data->addr + (y * data->line_length + x * (data->bpp / 8));
+		return ;
+	dst = data->addr + (y * data->line_length + x * (data->bpp / 8));
 	*(unsigned int *)dst = color;
 }
 
-void draw_circle_on_image(t_data *data, int radius, int x, int y)
+void	draw_circle_on_image(t_data *data, int radius, int x, int y)
 {
-	int i = -radius;
+	int	i;
+	int	j;
+
+	i = -radius;
 	while (i <= radius)
 	{
-		int j = -radius;
+		j = -radius;
 		while (j <= radius)
 		{
 			if (i * i + j * j <= radius * radius)
@@ -36,4 +41,3 @@ void draw_circle_on_image(t_data *data, int radius, int x, int y)
 		i++;
 	}
 }
-
