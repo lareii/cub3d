0a1ff18ad079842770130dbb7939ca873fe3d094
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2025-06-28 14:59:49 +0300

    refactor: simplify movement handling by consolidating key processing into a single function

--- src/render/handle_movement.c
+++ src/render/handle_movement.c
@@ -6,82 +6,43 @@
 /*   By: ahekinci <ahekinci@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 14:25:11 by ahekinci          #+#    #+#             */
-/*   Updated: 2025/06/28 14:25:54 by ahekinci         ###   ########.fr       */
+/*   Updated: 2025/06/28 14:59:40 by ahekinci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static void handle_movement_keys(t_data *data, double move_speed, int sign_x, int sign_y)
+{
+	double	new_x;
+	double	new_y;
+
+	if (sign_x == sign_y)
+	{
+		new_x = data->player->pos_x + ((data->player->dir_x * move_speed) * sign_x);
+		new_y = data->player->pos_y + ((data->player->dir_y * move_speed) * sign_y);
+	}
+	else
+	{
+		new_x = data->player->pos_x + ((data->player->dir_y * move_speed) * sign_x);
+		new_y = data->player->pos_y + ((data->player->dir_x * move_speed) * sign_y);
+	}
+	if (data->map->data[(int)data->player->pos_y][(int)new_x] != '1')
+		data->player->pos_x = new_x;
+	if (data->map->data[(int)new_y][(int)data->player->pos_x] != '1')
+		data->player->pos_y = new_y;
+}
+
 void	handle_movement(t_data *data)
 {
 	double	move_speed = data->mlx->frame_time * MOVE_SPEED_FACTOR;
-	double	new_x, new_y;
-	double	pos_x = data->player->pos_x;
-	double	pos_y = data->player->pos_y;
-	double	dir_x = data->player->dir_x;
-	double	dir_y = data->player->dir_y;
 
 	if (data->mlx->keys->key_w)
-	{
-		new_x = pos_x + dir_x * move_speed;
-		new_y = pos_y + dir_y * move_speed;
-		if (data->map->data[(int)pos_y][(int)new_x] != '1')
-			data->player->pos_x = new_x;
-		if (data->map->data[(int)new_y][(int)pos_x] != '1')
-			data->player->pos_y = new_y;
-	}
+		handle_movement_keys(data, move_speed, 1, 1);
 	if (data->mlx->keys->key_a)
-	{
-		new_x = pos_x + dir_y * move_speed;
-		new_y = pos_y - dir_x * move_speed;
-		if (data->map->data[(int)pos_y][(int)new_x] != '1')
-			data->player->pos_x = new_x;
-		if (data->map->data[(int)new_y][(int)pos_x] != '1')
-			data->player->pos_y = new_y;
-	}
+		handle_movement_keys(data, move_speed, 1, -1);
 	if (data->mlx->keys->key_s)
-	{
-		new_x = pos_x - dir_x * move_speed;
-		new_y = pos_y - dir_y * move_speed;
-		if (data->map->data[(int)pos_y][(int)new_x] != '1')
-			data->player->pos_x = new_x;
-		if (data->map->data[(int)new_y][(int)pos_x] != '1')
-			data->player->pos_y = new_y;
-	}
+		handle_movement_keys(data, move_speed, -1, -1);
 	if (data->mlx->keys->key_d)
-	{
-		new_x = pos_x - dir_y * move_speed;
-		new_y = pos_y + dir_x * move_speed;
-		if (data->map->data[(int)pos_y][(int)new_x] != '1')
-			data->player->pos_x = new_x;
-		if (data->map->data[(int)new_y][(int)pos_x] != '1')
-			data->player->pos_y = new_y;
-	}
+		handle_movement_keys(data, move_speed, -1, 1);
 }
-
-// static void handle_movement_keys(t_data *data, double move_speed, int sign_x, int sign_y)
-// {
-// 	double	new_x;
-// 	double	new_y;
-
-// 	new_x = data->player->pos_x + ((data->player->dir_x * move_speed) * sign_x);
-// 	new_y = data->player->pos_y + ((data->player->dir_y * move_speed) * sign_y);
-// 	if (data->map->data[(int)data->player->pos_y][(int)new_x] != '1')
-// 		data->player->pos_x = new_x;
-// 	if (data->map->data[(int)new_y][(int)data->player->pos_x] != '1')
-// 		data->player->pos_y = new_y;
-// }
-
-// void	handle_movement(t_data *data)
-// {
-// 	double	move_speed = data->mlx->frame_time * MOVE_SPEED_FACTOR;
-
-// 	if (data->mlx->keys->key_w)
-// 		handle_movement_keys(data, move_speed, 1, 1);
-// 	if (data->mlx->keys->key_a)
-// 		handle_movement_keys(data, move_speed, 1, -1);
-// 	if (data->mlx->keys->key_s)
-// 		handle_movement_keys(data, move_speed, -1, -1);
-// 	if (data->mlx->keys->key_d)
-// 		handle_movement_keys(data, move_speed, -1, 1);
-// }
