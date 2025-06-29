834c63c268ce66d093b17bfe4722a0023b32eec9
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2025-06-29 14:10:17 +0300

    fix: improve error handling in RGB parsing functions

--- src/parser/parse_rgb.c
+++ src/parser/parse_rgb.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_rgb.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebabaogl <ebabaogl@student.42kocaeli.co    +#+  +:+       +#+        */
+/*   By: ahekinci <ahekinci@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 23:19:17 by ahekinci          #+#    #+#             */
-/*   Updated: 2025/06/28 19:34:51 by ebabaogl         ###   ########.fr       */
+/*   Updated: 2025/06/29 14:06:53 by ahekinci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,24 @@ static int	parse_rgb_value(char *str)
 {
 	int	err;
 	int	value;
+	int i;
 
 	err = 0;
 	value = 0;
+	i = 0;
 	if (str)
 	{
+		while (str[i])
+		{
+			if (str[i] == ' ')
+			{
+				i++;
+				continue;
+			}
+			if (str[i] < '0' || str[i] > '9')
+				return (-1);
+			i++;
+		}
 		value = ft_atol(str, &err);
 		if (err == -1 || value < 0 || value > 255)
 			return (-1);
@@ -34,6 +47,23 @@ static int	parse_rgb_value(char *str)
 	return (value);
 }
 
+static int	count_char(const char *str, char c)
+{
+	int	count;
+
+	count = 0;
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		if (*str == c)
+			count++;
+		str++;
+	}
+	return (count);
+}
+
+#include <stdio.h>
 int	str_to_rgb(char *str)
 {
 	char	**rgb_values;
@@ -42,12 +72,14 @@ int	str_to_rgb(char *str)
 	int		b;
 
 	if (!str)
-		return (0);
+		return (-1);
+	if (count_char(str, ',') != 2)
+		return (-1);
 	rgb_values = ft_split(str, ',');
 	if (!rgb_values)
 	{
 		free(str);
-		return (0);
+		return (-1);
 	}
 	r = parse_rgb_value(rgb_values[0]);
 	g = parse_rgb_value(rgb_values[1]);
@@ -56,9 +88,10 @@ int	str_to_rgb(char *str)
 	{
 		free_str_arr(rgb_values);
 		free(str);
-		return (0);
+		return (-1);
 	}
 	free_str_arr(rgb_values);
 	free(str);
+	printf("r: %d g: %d b: %d\n", r, g, b);
 	return (rgb_to_int(r, g, b));
 }
